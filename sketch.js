5e3eff6685251ce8a6ca601faf11fa19560c68fb
michael-dart340 <113469950+michael-dart340@users.noreply.github.com>
2022-10-11 16:46:30 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+ function setup() {
+  createCanvas(400, 400);
+}  
+ellipsesizes=10
+ellipsesize=150;
+function draw() {
+  background(232,43,25);
+  
+  fill (150, 210, 222)
+rect (0,350, 310,200)
+  
+  fill (15,15,15)
+rect (300,300, 200,200);
+
+  fill (150, 90, 200)
+ellipse (175, 350, 150, 40)
+  
+  fill(244,208,63)
+  ellipsesize=ellipsesize+1;
+  ellipsesizes= ellipsesizes +1 ;
+ellipse (ellipsesize, ellipsesizes, 50, 50)
+   
+  
+}
